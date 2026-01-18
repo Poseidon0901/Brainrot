3cdb736124b6237d44b8dccec8a6a21f98a9a08c
Poseidon0901 <132719072+Poseidon0901@users.noreply.github.com>
2026-01-18 13:01:45 +0800

    Refactor brainrot.py with new functions

--- brainrot.py
+++ brainrot.py
@@ -1,28 +1,17 @@
-#CATS
+import math, cmath, random, time
+def 嬩笘鐣屼笂鏈(閫欐槸涓, 鍊):
+    鍊 += random.randint(int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real), -int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
+    if 鍊 <= 0:
+        return random.choice([閫欐槸涓, 閫欐槸涓[::-1], "🐱"])
+    鐨凱 = "".join(str(i) for i in list(""))
+    鎴戞槸鍌婚 = "🐱"
+    for 鍌婚 in range(len(閫欐槸涓)):
+        鎴戞 = len(閫欐槸涓) + int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real) + int((math.e ** (1j * math.pi)).real) * ord(鎴戞槸鍌婚)//128049 * 鍌婚
+        鐨凱 += list(閫欐槸涓)[鎴戞] + str(random.choice(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "🐱"]))
+    return 嬩笘鐣屼笂鏈(鐨凱, 鍊 + int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
 
-
-import math
-import cmath
-import random
-import time
-
-def stup1d_reverse(string, c):
-    c += random.randint(int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real), -int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
-    if c <= 0:
-        return random.choice([string, string[::-1], "🐱"])
-    uwu = list(string)
-    a = list("")
-    huh = "".join(str(i) for i in a)
-    wtf_is_this = "𘚠"
-    stop_imaging_this_number = cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)
-    for i in range(len(string)):
-        ind = len(string) + int((stop_imaging_this_number * stop_imaging_this_number).real) + int((math.e ** (1j * math.pi)).real) * ord(wtf_is_this)//100000 * i
-        huh += uwu[ind] + str(random.choice(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "🐱"]))
-    return stup1d_reverse(huh, c + int((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
-
-for i in range(random.randint(1, 200)):
-    stupidest_string = stup1d_reverse(stup1d_reverse(stup1d_reverse("Advanced Mathematics", i), 9), 2)[::-1]
-    for j in stupidest_string:
-        print(j, end="")
-        time.sleep(random.uniform((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real + 1, -(cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
-    print()
+闆ｉ柋璁 = 嬩笘鐣屼笂鏈(嬩笘鐣屼笂鏈(嬩笘鐣屼笂鏈("Advanced Mathematics", 8), 9), 2)[::-1]
+for 槸 in 闆ｉ柋璁:
+    print(槸, end="")
+    time.sleep(random.uniform((cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real + 1, -(cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real) * cmath.sqrt((1j * 1j - math.e ** (1j * math.pi) - 1).real)).real))
+print()
